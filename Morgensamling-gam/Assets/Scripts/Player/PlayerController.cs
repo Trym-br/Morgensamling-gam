5195927909509f755029691c160c47aa6c5878a4
Trym-br <trym.brandal.arhaug@gmail.com>
2025-01-07 11:08:29 +0100

    Basic Godawful player

---
+++ Morgensamling-gam/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 7f;
+ 
+    public bool playerIsGrounded;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public Vector2 groundBoxSize = new Vector2(0.8f,0.2f);
+    
+    private InputActions _input;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _input = GetComponent<InputActions>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        playerIsGrounded = Physics2D.OverlapBox(groundCheck.position, groundBoxSize, 0f, whatIsGround);
+        
+        if (_input.Jump && playerIsGrounded)
+        {
+            _rigidbody2D.linearVelocityY = jumpSpeed;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(groundCheck.position, groundBoxSize);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocityX = _input.Horizontal * moveSpeed;
+    }
+}
